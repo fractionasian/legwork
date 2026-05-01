40629d50d7d20b6e9c6ad143102f6f498405a6c1
Peter B. Nguyen <18570985+fractionasian@users.noreply.github.com>
2026-05-01 10:33:36 +0800

    chore: bump sw CACHE_NAME for spinner ux release

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CACHE_NAME is auto-bumped by .github/workflows/bump-sw.yml on push to main.
-var CACHE_NAME = "legwork-f4dea8a8";
+var CACHE_NAME = "legwork-spinneruv";
 var APP_SHELL = [
     "./",
     "./index.html",
