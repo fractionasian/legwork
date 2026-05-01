cba77ab3a874e965c515683a72cc3a8410aee0e4
Peter B. Nguyen <18570985+fractionasian@users.noreply.github.com>
2026-05-01 11:15:58 +0800

    fix: stabilise loading-ellipsis layout with clip-path animation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CACHE_NAME is auto-bumped by .github/workflows/bump-sw.yml on push to main.
-var CACHE_NAME = "legwork-a6ea11fc";
+var CACHE_NAME = "legwork-stableellipsis";
 var APP_SHELL = [
     "./",
     "./index.html",
